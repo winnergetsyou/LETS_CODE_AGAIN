7caba5463c866447e901fb6bf9be5d90ccb7bad7
winnergetsyou <97407880+winnergetsyou@users.noreply.github.com>
2023-01-20 01:13:23 -0500

    Update Recursion_factorial_number.py

--- Python_Preparation/Recursion_factorial_number.py
+++ Python_Preparation/Recursion_factorial_number.py
@@ -13,6 +13,8 @@ def Factorial_num(n):
   # (n-1) is the factor in that equation which decreases to meet the if equation to cease !
 
   return n * Factorial_num(n-1)
+
+  # NOTE: Recursion can be used without return and store the value in the variable like Result and print it... not necessarily , it has to be on the return statement
 	
 	
 
