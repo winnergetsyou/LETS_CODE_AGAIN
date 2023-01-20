99f20c66a65f55cf906a5e538330dff251347928
winnergetsyou <97407880+winnergetsyou@users.noreply.github.com>
2023-01-20 01:09:56 -0500

    Create Recursion_factorial_number.py

---
+++ Python_Preparation/Recursion_factorial_number.py
@@ -0,0 +1,22 @@
+# Find the factorial of the number 
+
+def Factorial_num(n):
+  
+  
+  # This is the BASE Condition Recursion calling stops once condition is met ... 
+  # To avoid overloading of Stack memory, make sure to sure the function that works towards the base condtion to cease or Infinite loop !
+  
+  if(n==1):
+  	return(n)
+  
+  # Below is the recursion calling n * Factorial_num(n-1)
+  # (n-1) is the factor in that equation which decreases to meet the if equation to cease !
+
+  return n * Factorial_num(n-1)
+	
+	
+
+# n is the user input
+n = 9
+#function calling and prints the data to the screen 
+print(printFun(n))
