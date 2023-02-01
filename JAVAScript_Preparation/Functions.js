4ffabba35b48d1e2803524ae4abc45e0b0d63f83
winnergetsyou <97407880+winnergetsyou@users.noreply.github.com>
2023-02-01 08:09:46 -0500

    Update Functions.js
    JavaScript Function

--- JAVAScript_Preparation/Functions.js
+++ JAVAScript_Preparation/Functions.js
@@ -1,1 +1,59 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1> FUNCTION </h1>
+<h2>
+function add(a,b)
+{
+return a+b;
+}
+</h2>
+
+The number one is 
+<p id="one">20</p>
+
+The number two is 
+<p id="two">25</p>
+
+After Addition number is 
+<p id="three"></p>
+
+<i>1. document.getElementById.html gets the html content <br>
+2. parseInt converts html element type to integer!</i>
+
+<script>
+
+function add(a,b)
+{
+return a+b;
+}
+y=document.getElementById("one").innerHTML;
+z=document.getElementById("two").innerHTML;
+
+document.getElementById("three").innerHTML=add(parseInt(y),parseInt(z));
+
+</script>
+
+</body>
+</html>
+
+
+
+
+<!--
+ FUNCTION
+function add(a,b) { return a+b; }
+The number one is
+
+20
+The number two is
+
+25
+After Addition number is
+
+45
+1. document.getElementById.html gets the html content
+2. parseInt converts html element type to integer!
+  -->
 
